e19351cfa2cb2194c740bdac5a8397929d5110da
MinhTris <boyds_24854@yahoo.com.vn>
2022-09-25 09:18:56 +0700

    C/CPP UPDATE

---
+++ C/testC.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+
+int main() {
+	// Declare character variable c and assign value 'a' to c
+	char c = '\usa';
+	// Display c on the screen
+	printf("%c", c);
+	return 0;
+}
